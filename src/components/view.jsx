b37140263a6925177f6c6c2e695962b87914a574
quyuyi <quyuyi@umich.edu>
2020-03-20 17:08:50 -0400

    draw clickable slots

--- src/components/view.jsx
+++ src/components/view.jsx
@@ -6,6 +6,7 @@ const ParkingLotsDB = require('../db.js');
 class View extends Component {
     constructor(props) {
         super(props);
+        this.items = [];
         this.state = {
             layout: ParkingLotsDB[0].layout,
         };
@@ -23,10 +24,13 @@ class View extends Component {
 
     render() {
         return (
-            <div id='grey-bkg'>
-                <canvas ref = { c => this.canvas = c; }
+            <div className='grey-bkg'>
+                <canvas ref = { c => this.canvas = c }
                     id="my-canvas">
                 </canvas>
+                <div>
+                    <img id='ban' src='public/images/banning_sign.png'></img>
+                </div>
             </div>
         );
     }
@@ -42,37 +46,56 @@ class View extends Component {
     };
 
     draw = () => {
+        console.log("clear");
         this.clear();
-        const { pl } = this.props;
-        const [widthRatio, heightRatio] = [this.width / pl.width, this.height / pl.height];
-        let path = new paper.CompoundPath({
-            children: [
-                new paper.Path.Line({
-                    from: [10, 0],
-                    to: [10, 100],
-                }),
-                new paper.Path.Line({
-                    from: [60, 0],
-                    to: [60, 100],
-                }),
-                new paper.Path.Rectangle({
-                    from: [10, 0],
-                    to: [60, 100],
-                    opacity: 0,
-                })
-            ],
-            strokeColor: 'black',
-            fillColor: 'white',
-        });
-        path.onClick = function(event) {
-            console.log("click");
-        }
-        paper.view.draw();
+        // const { pl } = this.props;
+        // const [widthRatio, heightRatio] = [this.width / pl.parkSize.width, this.height / pl.parkSize.height];
+        let [l, w] = [this.state.layout.slotSize.length, this.state.layout.slotSize.width];
+        this.state.layout.slots.forEach( p => {
+            let path = new paper.CompoundPath({
+                children: [
+                    new paper.Path.Line({
+                        from: p.coord,
+                        to: [p.coord[0], p.coord[1]+l],
+                    }),
+                    new paper.Path.Line({
+                        from: [p.coord[0]+w, p.coord[1]],
+                        to: [p.coord[0]+w, p.coord[1]+l],
+                    }),
+                    new paper.Path.Rectangle({
+                        from: p.coord,
+                        to: [p.coord[0]+w, p.coord[1]+l],
+                        opacity: 0,
+                    })
+                ],
+                strokeColor: 'black',
+                fillColor: 'white',
+            });
+            this.items.push(path);
+            if (p.occupied) {
+                let raster = new paper.Raster('ban');
+                raster.position = [p.coord[0]+0.5*w, p.coord[1]+0.5*l];
+                raster.scale(0.03);
+                raster.onClick = function(e) {
+                    console.log("click on the sign");
+                }
+                path.onClick = function(e) {
+                    console.log("click on banned slot");
+                }
+            } else {
+                path.onClick = function(e) {
+                    console.log("click on unoccupied slot");
+                }
+            }
+            paper.view.draw();
+        })
         return this;
     };
 
     clear = () => {
-
+        this.items.forEach( p => p.remove());
+        this.items = [];
+        paper.view.update();
     };
 }
 
