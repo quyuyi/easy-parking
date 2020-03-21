212ff970e8812bc9a204a89c61c9d1e8a7a8ebaa
rexcheng <rexcheng@umich.edu>
2020-03-21 14:57:28 -0400

    finished parking lot drawing

--- src/components/view.jsx
+++ src/components/view.jsx
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import { paper } from 'paper';
 
+const { Path, Point, Raster, Group } = paper;
 const ParkingLotsDB = require('../db.js');
 
 class View extends Component {
     constructor(props) {
         super(props);
         this.items = [];
-        this.state = {
-            layout: ParkingLotsDB[0].layout,
-        };
+        this.layout = ParkingLotsDB[1].layout;
     }
 
     componentDidMount() {
@@ -26,17 +25,17 @@ class View extends Component {
         return (
             <div className='grey-bkg'>
                 <canvas ref = { c => this.canvas = c }
-                    id="my-canvas">
+                    id='parking-lot-view'
+                    resize='true'>
                 </canvas>
-                <div>
-                    <img id='ban' src='public/images/banning_sign.png'></img>
+                <div className='hidden'>
+                    <img id='occupied' src='/public/images/occupied.png'></img>
+                    <img id='vacant' src='/public/images/vacant.png'></img>
+                    <img id='banned' src='/public/images/banned.png'></img>
+                    <img id='accessible'src='/public/images/accessible.png'></img>
                 </div>
             </div>
         );
-    }
-
-    handleClick() {
-        console.log("click!");
     }
 
     resize = () => {
@@ -45,58 +44,122 @@ class View extends Component {
         return this;
     };
 
-    draw = () => {
-        console.log("clear");
-        this.clear();
-        // const { pl } = this.props;
-        // const [widthRatio, heightRatio] = [this.width / pl.parkSize.width, this.height / pl.parkSize.height];
-        let [l, w] = [this.state.layout.slotSize.length, this.state.layout.slotSize.width];
-        this.state.layout.slots.forEach( p => {
-            let path = new paper.CompoundPath({
-                children: [
-                    new paper.Path.Line({
-                        from: p.coord,
-                        to: [p.coord[0], p.coord[1]+l],
-                    }),
-                    new paper.Path.Line({
-                        from: [p.coord[0]+w, p.coord[1]],
-                        to: [p.coord[0]+w, p.coord[1]+l],
-                    }),
-                    new paper.Path.Rectangle({
-                        from: p.coord,
-                        to: [p.coord[0]+w, p.coord[1]+l],
-                        opacity: 0,
-                    })
-                ],
-                strokeColor: 'black',
-                fillColor: 'white',
-            });
-            this.items.push(path);
-            if (p.occupied) {
-                let raster = new paper.Raster('ban');
-                raster.position = [p.coord[0]+0.5*w, p.coord[1]+0.5*l];
-                raster.scale(0.03);
-                raster.onClick = function(e) {
-                    console.log("click on the sign");
-                }
-                path.onClick = function(e) {
-                    console.log("click on banned slot");
-                }
-            } else {
-                path.onClick = function(e) {
-                    console.log("click on unoccupied slot");
-                }
-            }
-            paper.view.draw();
-        })
+    clear = () => {
+        this.items.forEach(item => item.remove());
+        this.items = [];
+        paper.view.update();
         return this;
     };
 
-    clear = () => {
-        this.items.forEach( p => p.remove());
-        this.items = [];
-        paper.view.update();
+    draw = () => {
+        this.clear();
+        // const { pl } = this.props;
+        // const [widthRatio, heightRatio] = [this.width / pl.parkSize.width, this.height / pl.parkSize.height];
+        const [widthRatio, heightRatio] = [this.width / this.layout.parkSize.width, this.height / this.layout.parkSize.height];
+        const [w, h] = [this.layout.slotSize.width * widthRatio, this.layout.slotSize.height * heightRatio]; // already scaled
+        this.layout.slots.forEach(ps => {
+            this.drawParkingSlot(ps, w, h, widthRatio, heightRatio);
+        });
+        paper.view.draw();
+        return this;
     };
+
+    drawParkingSlot = (ps, w, h, wr, hr) => {
+        const [x, y] = [ps.x * wr, ps.y * hr];
+        let g = new Group([
+            this.drawContour(x, y, w, h, ps.orient),
+            this.drawFill(x, y, w, h, ps.orient),
+            this.drawSign(x, y, w, h, ps.orient, ps.state)
+        ]);
+        g.onMouseEnter = function(e) {
+            this.children[1].selected = true;
+        };
+        g.onMouseLeave = function(e) {
+            this.children[1].selected = false;
+        };
+        this.items.push(g);
+        return this;
+    };
+
+    drawContour(x, y, w, h, orient) {
+        let contour = new Path();
+        contour.strokeColor = 'black';
+        switch (orient) {
+            case 'up':
+                contour.add(new Point(x, y));
+                contour.add(new Point(x, y + h));
+                contour.add(new Point(x + w, y + h));
+                contour.add(new Point(x + w, y));
+                break;
+
+            case 'down':
+                contour.add(new Point(x, y + h));
+                contour.add(new Point(x, y));
+                contour.add(new Point(x + w, y));
+                contour.add(new Point(x + w, y + h));
+                break;
+
+            case 'left': // width / height swapped
+                contour.add(new Point(x, y));
+                contour.add(new Point(x + h, y));
+                contour.add(new Point(x + h, y + w));
+                contour.add(new Point(x, y + w));
+                break;
+
+            case 'right': // width / height swapped
+                contour.add(new Point(x + h, y));
+                contour.add(new Point(x, y));
+                contour.add(new Point(x, y + w));
+                contour.add(new Point(x + h, y + w));
+                break;
+        }
+        return contour;
+    }
+
+    drawFill(x, y, w, h, orient) {
+        switch (orient) {
+            case 'up':
+            case 'down':
+                return new Path.Rectangle({
+                    point: [x, y],
+                    size: [w, h],
+                    opacity: 0,
+                    fillColor: 'white'
+                });
+
+            default: // left / right
+                return new Path.Rectangle({
+                    point: [x, y],
+                    size: [h, w],
+                    opacity: 0,
+                    fillColor: 'white'
+                });
+        }
+    }
+
+    drawSign(x, y, w, h, orient, state) {
+        let sign = new Raster(state);
+        switch (orient) {
+            case 'up':
+            case 'down':
+                sign.position = [x + w / 2, y + h / 2];
+                break;
+
+            default: // left / right
+                sign.position = [x + h / 2, y + w / 2];
+        }
+        sign.scale(w / 100);
+        sign.onMouseEnter = e => {
+            this.canvas.style.cursor = 'pointer';
+        };
+        sign.onMouseLeave = e => {
+            this.canvas.style.cursor = 'default';
+        };
+        sign.onClick = function(e) {
+            console.log('sign clicked');
+        };
+        return sign;
+    }
 }
 
 export default View;
