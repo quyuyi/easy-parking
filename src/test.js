c1f31c9de2540a7ae661b62e70b01acabd80ec7c
quyuyi <quyuyi@umich.edu>
2020-03-26 17:41:26 -0400

    add db to 9

---
+++ src/test.js
@@ -0,0 +1,9 @@
+slots: [
+    { x: 1255, y: 730, orient: 'left', state: 'occupied', property: 'vacant' },
+    { x: 1255, y: 680, orient: 'left', state: 'occupied', property: 'vacant' },
+    { x: 1255, y: 630, orient: 'left', state: 'occupied', property: 'vacant' },
+    { x: 1255, y: 580, orient: 'left', state: 'occupied', property: 'vacant' },
+    { x: 1255, y: 530, orient: 'left', state: 'occupied', property: 'vacant' },
+    { x: 1255, y: 480, orient: 'left', state: 'occupied', property: 'vacant' },
+
+]
