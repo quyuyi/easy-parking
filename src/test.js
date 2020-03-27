0e65a915246a173aef6f475a6124e924be89489b
rexcheng <rexcheng@umich.edu>
2020-03-27 11:37:58 -0400

    added NC31 Parking Lot

--- src/test.js
+++
@@ -1,9 +0,0 @@
-slots: [
-    { x: 1255, y: 730, orient: 'left', state: 'occupied', property: 'vacant' },
-    { x: 1255, y: 680, orient: 'left', state: 'occupied', property: 'vacant' },
-    { x: 1255, y: 630, orient: 'left', state: 'occupied', property: 'vacant' },
-    { x: 1255, y: 580, orient: 'left', state: 'occupied', property: 'vacant' },
-    { x: 1255, y: 530, orient: 'left', state: 'occupied', property: 'vacant' },
-    { x: 1255, y: 480, orient: 'left', state: 'occupied', property: 'vacant' },
-
-]
